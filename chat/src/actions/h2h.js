f53c9c8a0e86c49a227078aaa9037ec8a9a662a3
Valérian Galliat <val@codejam.info>
2017-12-07 13:45:01 -0500

    Fix #h2h

--- chat/src/actions/h2h.js
+++ chat/src/actions/h2h.js
@@ -3,6 +3,8 @@ module.exports = function streak ({ bot, socket, message }) {
   if (players.length < 2) players.push(message.author)
   if (players.length < 2) return message.send('Need two users')
 
+  const [playerOne, playerTwo] = players
+
   socket.emit('head-to-head', playerOne, playerTwo, data => {
     message.send(`${playerOne.name} ${data.player_one} - ${data.player_two} ${playerTwo.name}`)
   })
