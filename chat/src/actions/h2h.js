f987bbbfd01063e7d02b4aff5922fe6af27d1230
Valérian Galliat <val@codejam.info>
2018-01-12 16:55:12 -0500

    Puts self first in head to head command

--- chat/src/actions/h2h.js
+++ chat/src/actions/h2h.js
@@ -1,6 +1,6 @@
 module.exports = function streak ({ bot, socket, message }) {
   const players = message.mentions()
-  if (players.length < 2) players.push(message.author)
+  if (players.length < 2) players.unshift(message.author)
   if (players.length < 2) return message.send('Need two users')
 
   const [playerOne, playerTwo] = players
