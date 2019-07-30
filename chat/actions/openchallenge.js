d164c3c0c32823a97d47affea7db9bfefabfd574
Val <val@codejam.info>
2019-07-30 15:14:08 -0400

    Gender neutral copy

--- chat/actions/openchallenge.js
+++ chat/actions/openchallenge.js
@@ -2,7 +2,7 @@ module.exports = function openchallenge ({ bot, addRequest, message, flags }) {
   const challenger = message.author
   const mentions = message.mentions()
 
-  if (mentions.length) return message.send('It\'s an open challenge man...')
+  if (mentions.length) return message.send('It\'s an open challenge mate...')
 
   addRequest({ challenger, challengee: null, message, unranked: flags.forfun })
 }
